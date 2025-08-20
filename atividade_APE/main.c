3b8a07ed28235f55a30fd119abd12ab6aabe1ccc
Augusto <guilhermesantos172007@gmail.com>
2025-08-20 16:58:02 -0300

    Add files via upload

---
+++ atividade_APE/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("--------------------------------------\n");
+	printf("|                                    |\n");
+	printf("| Disciplina: Sistema Computacionais |\n");
+	printf("| Turma: GPE17N20241                 |\n");
+	printf("| Nome: Guilherme Augusto            |\n");
+	printf("| Matricula: UC25100900              |\n");
+	printf("|                                    |\n");
+	printf("--------------------------------------\n");
+	return 0;
+}
