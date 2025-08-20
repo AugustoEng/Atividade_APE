4e545fee8d747b5f1a9ba96bca52aef10d3bbab5
Augusto <guilhermesantos172007@gmail.com>
2025-08-20 17:00:17 -0300

    Add files via upload

--- atividade_APE/main.c
+++ atividade_APE/main.c
@@ -8,6 +8,7 @@ int main()
 	printf("| Turma: GPE17N20241                 |\n");
 	printf("| Nome: Guilherme Augusto            |\n");
 	printf("| Matricula: UC25100900              |\n");
+	printf("| Altura: 1.88                       |\n");
 	printf("|                                    |\n");
 	printf("--------------------------------------\n");
 	return 0;
